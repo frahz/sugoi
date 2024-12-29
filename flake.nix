201780fdc2e0742f178e90b67288497f1a11bfec
frahz <me@frahz.dev>
2024-12-28 18:55:35 -0800

    nix: remove deps on `flake-utils` and update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,59 +2,61 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
+    self,
     nixpkgs,
     rust-overlay,
-    flake-utils,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-
-        buildInputs = with pkgs; [
-          rust-bin.stable.latest.default
-          openssl
-          pkg-config
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    overlays = [(import rust-overlay)];
+    forEachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsForEach = forEachSystem (system: import nixpkgs {inherit system overlays;});
+  in {
+    devShells = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.rust-bin.stable.latest.default
         ];
-      in {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          inherit (cargoToml.package) name version;
+      };
+    });
 
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            allowBuiltinFetchGit = true;
-          };
-          nativeBuildInputs = [pkgs.makeWrapper];
+    packages = forEachSystem (system: let
+      pkgs = pkgsForEach.${system};
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    in {
+      default = pkgs.rustPlatform.buildRustPackage {
+        inherit (cargoToml.package) name version;
 
-          postInstall = ''
-            mkdir -p $out/share
-            cp -r assets $out/share
-            wrapProgram $out/bin/sugoi \
-              --set ASSETS_DIR $out/share/assets
-          '';
-
-          meta = with pkgs.lib; {
-            description = "small web server for waking up and putting my server to sleep.";
-            homepage = "https://git.iatze.cc/frahz/sugoi";
-            changelog = "https://git.iatze.cc/frahz/sugoi/releases/tag/v${version}";
-            license = licenses.mit;
-          };
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          allowBuiltinFetchGit = true;
         };
+        nativeBuildInputs = [pkgs.makeWrapper];
 
-        devShells.default = with pkgs;
-          mkShell {
-            inherit buildInputs;
-          };
-      }
-    );
+        postInstall = ''
+          mkdir -p $out/share
+          cp -r assets $out/share
+          wrapProgram $out/bin/sugoi \
+            --set ASSETS_DIR $out/share/assets
+        '';
+
+        meta = with pkgs.lib; {
+          description = "small web server for waking up and putting my server to sleep.";
+          homepage = "https://git.iatze.cc/frahz/sugoi";
+          changelog = "https://git.iatze.cc/frahz/sugoi/releases/tag/v${version}";
+          license = licenses.mit;
+        };
+      };
+    });
+  };
 }
